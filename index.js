3e2920d84ca010d210ded29d86cb5a505bb2c634
Kevin Barabash <kevinb@khanacademy.org>
2017-05-29 16:02:20 -0400

    export 'primeFactorization'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import evaluate from './lib/evaluate'
-import {gcd, lcm} from './lib/functions'
+import {gcd, lcm, primeFactorization} from './lib/functions'
 
 export {
     gcd,
     lcm,
+    primeFactorization,
+
     evaluate as default,
 }
